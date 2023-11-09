3b913b60667704a24b11185b9ab9d4eb55be37c8
bukwildman <62768218+bukwildman@users.noreply.github.com>
2023-11-09 15:14:37 -0500

    more

--- controller.js
+++ controller.js
@@ -1,3 +1,38 @@
+// In your controller.js
 app.controller('MyController', function($scope) {
+    $scope.showProfile = false;
+    $scope.showExplore = false;
+    $scope.showChat = false;
+
+    $scope.openProfile = function() {
+        $scope.showProfile = true;
+        $scope.showExplore = false;
+        $scope.showChat = false;
+    };
+
+    $scope.openExplore = function() {
+        $scope.showProfile = false;
+        $scope.showExplore = true;
+        $scope.showChat = false;
+    };
+
+    $scope.openChat = function() {
+        $scope.showProfile = false;
+        $scope.showExplore = false;
+        $scope.showChat = true;
+    };
+    // Sample data
+    $scope.userName = "John Doe";
+    $scope.userEmail = "johndoe@example.com";
+    $scope.userAge = 30;
+    $scope.userCity = "New York";
+    $scope.userFood = "Pizza";
+
+    $scope.chats = [
+        { name: "Alice", lastMessage: "See you soon!"},
+        { name: "Bob", lastMessage: "Thanks!"},
+        // ... other chats
+    ];
+    
 
 });
